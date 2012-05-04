3a68f0bef5bb71821b5c702cefe45466b8d0dd77
ziofab <ziofab@gmail.com>
2012-05-05 01:33:01 +0200

    Start...

--- 2012-05-04/exercise1.js
+++ 2012-05-04/exercise1.js
@@ -3,18 +3,37 @@
 ** Produce the model of a single wing in a local coordinate system.
 */
 
-var domain2 = DOMAIN([[0,1],[0,1]])([15,30]);
+var domainWing = DOMAIN([[0,1],[0,1]])([15,30]);	// Divide x axis into 15 intervals and y's into 30
 
 /* Create control point for BEZIER(S0) */
-var cpwing0 = [[10,0,0],[0,5,0],[0,-3,0],[5,2,0],[10,0,0]];
-var cpwing1 = [[10,0,10],[0,5,10],[0,-3,10],[5,2,10],[10,0,10]];
-var cpwing2 = [[10,0,20],[0,5,20],[0,-3,20],[5,2,20],[10,0,20]];
-var cpwing3 = [[10,0,30],[0,5,30],[0,-3,30],[5,2,30],[10,0,30]];
-var cpwing4 = [[5,0,40],[0,2.5,40],[0,-1.5,40],[2.5,1,40],[5,0,40]];
-var cpwing5 = [[0,0,50],[0,0,50],[0,0,50],[0,0,50],[0,0,50]];
+var cpWing0 = [[10,0,0],[0,5,0],[0,-3,0],[5,2,0],[10,0,0]];
+var cpWing1 = cpWing0.map(function (p) {return [p[0], p[1], p[2]+10]});
+var cpWing2 = cpWing0.map(function (p) {return [p[0], p[1], p[2]+20]});
+var cpWing3 = cpWing0.map(function (p) {return [p[0], p[1], p[2]+30]});
+var cpWing4 = cpWing0.map(function (p) {return [p[0], p[1], p[2]+40]});
+var cpWingEnd = cpWing4.map(function (p) {return [5, 0.8, p[2]]});
 
-var controls = AA(BEZIER(S0))([cpwing0,cpwing1,cpwing2,cpwing3,cpwing4,cpwing5]);
+var bCpW0 = BEZIER(S0)(cpWing0);
+var bCpW1 = BEZIER(S0)(cpWing1);
+var bCpW2 = BEZIER(S0)(cpWing2);
+var bCpW3 = BEZIER(S0)(cpWing3);
+var bCpW4 = BEZIER(S0)(cpWing4);
 
-var wing = BEZIER(S1)(controls);
-var surf = MAP(wing)(domain2);
+var bCpWEnd = BEZIER(S0)(cpWingEnd);
+
+var controlsWing = [bCpW0,bCpW1,bCpW2,bCpW3,bCpW4]
+// I
+//var controlsWing = AA(BEZIER(S0))([cpWing0,cpWing1,cpWing2,cpWing3,cpWing4]);
+
+var wing = BEZIER(S1)(controlsWing);
+var surf = MAP(wing)(domainWing);
+
+//Curves bCpW4 and bCpWEnd are used as parameters for BEZIER function.
+//BEZIER is used in transfinite manner: first argument is S1 (not S0),
+//									 	and like second argument the two curves bCpW4 and bCpWEnd.).
+//Note: two curves can generate a surface.
+var controlsWingEnd = BEZIER(S1)([bCpW4, bCpWEnd]);
+var surfaceWingEnd = MAP(controlsWingEnd)(domainWing);
+DRAW(surfaceWingEnd);
+
 DRAW(surf);
