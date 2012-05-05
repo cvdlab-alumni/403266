dfbf3e11eb292cdd3a13c182e66ed1286193fd80
ziofab <ziofab@gmail.com>
2012-05-05 14:09:31 +0200

    ex3...

--- 2012-05-04/exercise3.js
+++ 2012-05-04/exercise3.js
@@ -43,7 +43,7 @@ function buildSurface(cs, mirrorAxis)
 
 	csHorizontal = new Array();
 	
-	csHorizontal[0] = [[ 0.50, 0.10, 0.00], [ 0.00, 0.10, 0.00], [ 0.00, 0.00, 0.00], [ 0.00, 0.00, 0.00]];
+	csHorizontal[0] = [[ 0.50, 0.15, 0.00], [ 0.00, 0.10, 0.00], [ 0.00, 0.00, 0.00], [ 0.00, 0.00, 0.00]];
 	csHorizontal[1] = [[ 0.49, 0.20, 0.00], [ 0.00, 0.20, 0.00], [ 0.00, 0.00, 0.10], [ 0.00, 0.00,-0.10]];
 	csHorizontal[2] = [[ 0.48, 0.30, 0.00], [ 0.00, 0.30, 0.00], [ 0.00, 0.00, 0.10], [ 0.00, 0.00,-0.10]];
 	csHorizontal[3] = [[ 0.47, 0.40, 0.00], [ 0.00, 0.40, 0.00], [ 0.00, 0.00, 0.10], [ 0.00, 0.00,-0.10]];
@@ -56,22 +56,45 @@ function buildSurface(cs, mirrorAxis)
 
 	var horizontalStabilizerDx = buildSurface(csHorizontal, 2); // Mirroring on Z axis
 
-	
 	if (horizontalStabilizerDx)
 	{
-		var horizontalStabilizers = STRUCT([ horizontalStabilizerDx, S([0])([-1])(horizontalStabilizerDx)]);
-		var horizontalStabilizersAndElevators = STRUCT([ horizontalStabilizers, S([1])([-1])(horizontalStabilizers)]);
+		csVertical = new Array();
+		
+		csVertical[0] = [[ 0.00, 0.00, 1.00], [ 0.00, 0.00,-1.00], [ 0.00, 0.00, 0.00], [ 0.00, 0.00, 0.00]];
+		csVertical[1] = [[ 0.20, 0.00, 0.99], [ 0.20, 0.00,-0.99], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[2] = [[ 0.30, 0.00, 0.98], [ 0.30, 0.00,-0.98], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[3] = [[ 0.40, 0.00, 0.95], [ 0.40, 0.00,-0.95], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[4] = [[ 0.50, 0.00, 0.90], [ 0.50, 0.00,-0.90], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[5] = [[ 0.60, 0.00, 0.80], [ 0.60, 0.00,-0.80], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[6] = [[ 0.70, 0.00, 0.65], [ 0.70, 0.00,-0.65], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[7] = [[ 0.80, 0.00, 0.45], [ 0.80, 0.00,-0.45], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[8] = [[ 0.90, 0.00, 0.20], [ 0.90, 0.00,-0.20], [ 0.00, 0.10, 0.00], [ 0.00,-0.10, 0.00]];
+		csVertical[9] = [[ 0.91, 0.00, 0.00], [ 0.91, 0.00, 0.00], [ 0.00, 0.00, 0.00], [ 0.00,-0.00, 0.00]];
+	
+		var verticalStabilizers = buildSurface(csVertical, 1); // Mirroring on Y axis
+		if (verticalStabilizers)
+		{
+			var horizontalStabilizers = STRUCT([ horizontalStabilizerDx, S([0])([-1])(horizontalStabilizerDx)]);
+			var horizontalStabilizersAndElevators = STRUCT([ horizontalStabilizers, S([1])([-1])(horizontalStabilizers)]);
 
-
-		exports.stabilizers = horizontalStabilizersAndElevators;
-
-		// Draw
-		DRAW(horizontalStabilizersAndElevators);
-		return horizontalStabilizersAndElevators;
+			var verticalStabilizersAndRubber = STRUCT([ verticalStabilizers, S([0])([-1])(verticalStabilizers)]);
+			
+			var stabilizers = STRUCT([ horizontalStabilizersAndElevators, verticalStabilizersAndRubber ]);
+			
+			exports.stabilizers = stabilizers;
+	
+			// Draw
+			DRAW(stabilizers);
+			return stabilizers;
+		}
+		else
+		{
+			console.log("Error no surface verticalStabilizers!!!");
+		}			
 	}
 	else
 	{
-		console.log("Error no surface!!!");
+		console.log("Error no surface horizontalStabilizerDx!!!");
 	}
 
 }(this)) // "this" often is window (global variable)
