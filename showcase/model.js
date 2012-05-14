adca89a75c6892deac14418e06d93e9c3d67cd33
ziofab <ziofab@gmail.com>
2012-05-14 17:37:03 +0200

    King

--- showcase/model.js
+++ showcase/model.js
@@ -1,39 +1,47 @@
 /* 403266: Fabrizio Andreoli
 ** model.js
-** Model a chess pawn piece.
+** Model a chess king piece.
 */
 
 (function (exports) {
+	var scale = 1/187; // King object scale factor. Makes base into [1,1].
+	var cnubsKingRots = [[0,0], // [0,0] in order to have a closed object (at bottom).
+ [161,0], [161,1], [162,5], [175,11], [187,23], [186,30], [169,53], [176,75], [169,103],
+ [140,149], [141,164], [142,170], [144,178], [125,192], [77,448], [88,532], [105,533],
+ [120,538], [125,551], [120,568], [110,577], [115,588], [110,601], [109,602], [107,603],
+ [105,604], [95,611], [96,622], [105,633], [104,648], [97,656], [104,703], [115,726],
+ [125,750], [124,763], [120,769], [91,784],
+ [30,793], [25, 810], [15, 890], [15, 900], // Vertical part of cross of the cross of the king.
+ [0, 900] ]; // Closing the top of the king
 
-	 // Sequence of [Y,Z].
-	 // Model a NUBS curve that is rotated on X,Y planes to obtain a 3D object.
-	 // First and last must be Y=0 in order to have a closed object.
-	var cnubs = [[0,0], // [0,0] in order to have a closed object (at bottom).
+	// cnubsCross is used to create a cyl-like  object centered on Z axis, that makes the
+	// horizontal part of the cross of the king. Use the same scale factor of cnubsKingRots.
+	var cnubsCross = [[0,-40], [15,-40],  [15, 40], [0, 40] ];
+
+/*	var cnubsPawn = [[0,0], // [Radius = 0,] in order to have a closed object (at bottom).
 	             [44,0], // To build base, 0 on Z axis.
 	             [44,23],[32,24],[36,25],[39,28],[28,40],[20,58],[17,73],[21,90],[14,95],
 	             [19,101],[24,113],[20,130],[13,138],[8,141], [3,143],
-	             [0,143]]; // 0 on Y to make a closed (on top) object.
-	             // This data was manually taken by sk-scheda-10_html_60264dc2.gif in http://www.guzman.it/schedesketchup/scheda%2010/sk-scheda-10.html
+	             [0,143]]; // 0 on Radius to make a closed (on top) object.
+	var scale = 1/44; // Pawn object scale factor. Makes base into [1,1]. */
 
-	var scale = 1/44; // Object scale factor. Makes base into [1,1].
-
-	var powdivs = 3; // Object is calculated by 0 to (PI/2/(2^powdivs)) degree. Then is replicated with affine transformations.
-	var ndivs = 4; // Calculate ndivs NUBS curves into (PI/2/(2^powdivs)) degree.
-
-	function BuildObjectRotatingXyNUBS(cnubs, knots, powdivs, ndivs, scale)
+	function BuildObjectRotatingXyNUBS(cnubs, scale, powdivs, nNUBS, knots, domain2)
 	{
-		// Create an object by a rotating NUBS
-		// INPUT cnubs: contains control points for NUBS, remember that X coordinate is not present...([[,]] not [[,,]])
-		// INPUT knots: knots for NUBS, if is it null then is calculated with standard formula...
-		// INPUT powdivs: PI/2 is divided by (2^powdivs): object is calculated by 0 to alfaK degree. Then is replicated with affine transformations.
-		// INPUT ndivs: Divide really calcultated curve into ndivs NUBS curves rotated by planes(X,Y)
+		// Create an object by a rotating NUBS. Only cnubs parameter is mandatory
+		// Model a NUBS curve that is rotated on X,Y planes to obtain a 3D object.
+
+		// INPUT cnubs: Array of [Radius,Z]. Contains control points for NUBS, remember that first coordinate is radius and then Z...other coordinates are ignored ([[,]] not [[,,]]). First and last must have Radius=0 in order to have a closed object.
 		// INPUT scale: Object scale factor
+		// INPUT powdivs: 2*PI is divided by (2^powdivs); only one BEZIER curve is calculated (by 0 to (2*PI/(2^powdivs)) degree). Then is replicated with affine transformations.
+		// INPUT nNUBS: BEZIER curve is calcultated on nNUBS NUBS curves (equally) rotated by planes(X,Y) from 0 to (2*PI/(2^powdivs)) degree.
+		// INPUT knots: knots for NUBS, if is it null then is calculated with standard formula...
+		// INPUT domain2: Domain for BEZIER curve
 		// OUTPUT surface built by passed parameters
 
-		// Domain should be passed as parameters?!?!?
-		var domain1 = INTERVALS(1)(cnubs.length*2);
-		var domain2 = DOMAIN([[0,1],[0,1]])([cnubs.length*3,ndivs]); 
-
+//		var domain1 = INTERVALS(1)(cnubs.length*2); // Only used for DEBUG printout
+		if (scale == null) scale = 1.0;
+		if (powdivs == null) powdivs = 5;
+		if (nNUBS == null) nNUBS = 4;
 		if (knots == null)
 		{
 			knots = new Array();
@@ -43,21 +51,22 @@
 			knots[i+3] = knots[i+4] = knots[i+5] = i+1;
 			for (; i>0; i--) knots[i+2] = i;
 		}
+		if (domain2 == null) domain2 = DOMAIN([[0,1],[0,1]])([cnubs.length*3,nNUBS]); 
 
 		var cc = new Array();
 	
 		var ndivangle = Math.pow(2, powdivs);
 	
-		var alfaK = Math.PI/2/ndivangle;
+		var alfaK = 2*Math.PI/ndivangle;
 
-		for (r=0; r<=ndivs; r++)
+		for (r=0; r<=nNUBS; r++)
 		{
-			var alfa = alfaK * r / ndivs;
+			var alfa = alfaK * r / nNUBS;
+			// cnubs array doesn't have X coordinate: create ctrlAlfa for NUBS()()()() function
 			var ctrlAlfa = AA(function(p) { return [p[0]*COS(alfa) * scale, p[0]*SIN(alfa) *scale, p[1] *scale] })(cnubs);
 			cc[r] = NUBS(S0)(2)(knots)(ctrlAlfa);
 		}
-/*
-// DEBUG
+/* // DEBUG
 var curve0 = COLOR([1,0,0])(MAP(cc[0])(domain1));
 DRAW(curve0);
 var curve1 = COLOR([0,1,0])(MAP(cc[1])(domain1));
@@ -69,25 +78,22 @@ DRAW(curve1);
 	
 		slices = new Array();
 		slices[0] = surfp;
-		for (c=1; c<ndivangle; c++)
+		for (c=1; c<=powdivs; c++)
 		{
-			slices[c] = ROTATE([0,1])(alfaK*c)(surfp);
+			slices[c] = STRUCT([slices[c-1], ROTATE([0,1])(alfaK*Math.pow(2, c-1))(slices[c-1])]);
 		}
-	
-		objectPIhalf = STRUCT(slices);
-		// Replicate [0..PI/2] on [PI/2..PI]
-		qII = S([0])([-1])(objectPIhalf)
-		qUP = STRUCT([objectPIhalf, qII])
-		// Replicate [0..PI] on [PI..2*PI]
-		qDW = S([1])([-1])(qUP)
-		return STRUCT([qUP,qDW]);
+		return STRUCT(slices);
 	}
 
-	var model = COLOR([1,1,1])( BuildObjectRotatingXyNUBS(cnubs, null, powdivs, ndivs, scale) );
+	var c = R([0,2])(PI/2)( BuildObjectRotatingXyNUBS(cnubsCross, scale) );
+	var zOffset = (cnubsKingRots[cnubsKingRots.length-1][1]-cnubsCross[1][0]*2)*scale;
+	cylcross =  T([2])([zOffset])( c );
 
-	exports.m =  model;
+	var model = STRUCT([BuildObjectRotatingXyNUBS(cnubsKingRots, scale), cylcross]);
+	var colorModel = COLOR([1,1,1])( model );
+
+	exports.m =  colorModel;
 
 	DRAW(model);
 			
 }(this)) // "this" often is window (global variable)
-
